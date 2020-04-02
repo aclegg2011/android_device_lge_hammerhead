18b60bcb8ddcd7f0d29f79b7f8dd41db1ebcf321
Aren Clegg <aclegg2011@gmail.com>
2020-04-02 19:13:36 +0000

    shamu: blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hammerhead.mk
+    $(LOCAL_DIR)/bliss_hammerhead.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_hammerhead-user \
-    lineage_hammerhead-userdebug \
-    lineage_hammerhead-eng
+    bliss_hammerhead-user \
+    bliss_hammerhead-userdebug \
+    bliss_hammerhead-eng
